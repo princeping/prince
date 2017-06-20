8c26a99549fc0b617b1e2bfbd5bd27fb2f5d2fda
princeping <wangzi3991@gmail.com>
2017-06-20 10:42:40 +0800

    利用广播变量实现多关键词过滤

--- sparkdemo/src/main/scala/com/prince/demo/spark/mytest/BroadCastDemo.scala
+++ sparkdemo/src/main/scala/com/prince/demo/spark/mytest/BroadCastDemo.scala
@@ -1,9 +1,9 @@
 package com.prince.demo.spark.mytest
 
-import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
 /**
+  * 利用广播变量进行多关键词过滤
   * Created by princeping on 2017/5/19.
   */
 object BroadCastDemo {
@@ -12,21 +12,22 @@ object BroadCastDemo {
     val spark = SparkSession.builder().master("local").appName("BroadCastDemo").getOrCreate()
     val sc = spark.sparkContext
 
-//    val broads = sc.broadcast(3)
-//    val lists = List(1,2,3,4,5)
-//    val listRDD = sc.parallelize(lists)
-//    val result = listRDD.filter(x=>x*broads.value>10)
-//    result.foreach(x=>println("增加后的结果:" + x))
+    val input = sc.textFile("C:\\Users\\Administrator\\Desktop\\test.txt")
+    val input1 = sc.textFile("C:\\Users\\Administrator\\Desktop\\tes.txt")
 
-//    val array:Array[String] = Array("POST", "GET")
-//    val broadcast = sc.broadcast(array)
-    val array:Array[String] = Array("POST", "GET")
-    val rdd:RDD[String] = sc.textFile("C:\\Users\\Administrator\\Desktop\\新建文件夹\\0430.log")
-//    val filterRDD = rdd.filter(line => line.split("\\|")(6).contains(array))
-//    filterRDD.repartition(1).saveAsTextFile("E:\\new")
-    for(i<-0 until array.length){
-      val filterRDD = rdd.filter(line => line.split("\\|")(6).contains(array(i)))
-    }
+    val broads = sc.broadcast(input1.collect())
+
+    val out = input.map(x => {
+      var str:String = ""
+      val filter = broads.value.foreach(y =>{
+        if(x.contains(y))
+          str = x
+      })
+      str
+    }).filter(_.length>0)
+
+    out.repartition(1).saveAsTextFile("C:\\Users\\Administrator\\Desktop\\out")
+
     spark.stop()
   }
 }
